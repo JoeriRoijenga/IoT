bf42d4c6edbb448a4cd1f73e266f623d634ed027
joeriroijenga <roijenga.joeri@gmail.com>
2020-11-08 13:19:32 +0100

    datetime import

--- data_handler.py
+++ data_handler.py
@@ -1,4 +1,5 @@
-from time import strftime, now
+from time import strftime
+from datetime import datetime
 import paho.mqtt.client as mqtt
 import sqlite3
 
@@ -22,7 +23,7 @@ def on_connect(client, userdata, flags, rc):
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    theTime = strftime("%Y-%m-%d %H:%M:%S", now().strftime("%Y-%m-%d %H:%M:%S"))
+    theTime = strftime("%Y-%m-%d %H:%M:%S", datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
 
     result = (theTime + "\t" + str(msg.payload))
     print(msg.topic + ":\t" + result)
