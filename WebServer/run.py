f21784f514915e2f7df7dab6c8c69dde3a259516
joeriroijenga <roijenga.joeri@gmail.com>
2020-11-08 14:13:39 +0100

    run.py changes

--- WebServer/run.py
+++ WebServer/run.py
@@ -1,33 +1,112 @@
-from flask import Flask, render_template, request
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+import io
+from flask import Flask, render_template, send_file, make_response, request
 app = Flask(__name__)
 import sqlite3
+conn=sqlite3.connect('../sensorsData.db')
+curs=conn.cursor()
 
-# Retrieve data from database
-def getData():
-	conn=sqlite3.connect('../espData.db')
-	curs=conn.cursor()
-	for row in curs.execute("SELECT * FROM ESP_data ORDER BY timestamp DESC LIMIT 1"):
+
+# Retrieve LAST data from database
+def getLastData():
+	for row in curs.execute("SELECT * FROM DHT_data ORDER BY timestamp DESC LIMIT 1"):
 		time = str(row[0])
 		temp = row[1]
 		hum = row[2]
-		print(time)
-		print(temp)
-		print(hum)
-	conn.close()
+	#conn.close()
 	return time, temp, hum
+
+
+def getHistData (numSamples):
+	curs.execute("SELECT * FROM DHT_data ORDER BY timestamp DESC LIMIT "+str(numSamples))
+	data = curs.fetchall()
+	dates = []
+	temps = []
+	hums = []
+	for row in reversed(data):
+		dates.append(row[0])
+		temps.append(row[1])
+		hums.append(row[2])
+	return dates, temps, hums
+
+
+def maxRowsTable():
+	for row in curs.execute("select COUNT(temp) from  DHT_data"):
+		maxNumberRows=row[0]
+	return maxNumberRows
+
+
+# define and initialize global variables
+global numSamples
+numSamples = maxRowsTable()
+    if (numSamples > 101):
+numSamples = 100
 
 
 # main route
 @app.route("/")
 def index():
-	time, temp, hum = getData()
+	time, temp, hum = getLastData()
 	templateData = {
-		'time': time,
-		'temp': temp,
-		'hum': hum
+	  	'time'	: time,
+		'temp'	: temp,
+      		'hum'	: hum,
+      		'numSamples'	: numSamples
 	}
 	return render_template('index.html', **templateData)
 
 
-if __name__ == "__main__":
-   app.run(host='0.0.0.0', port=80, debug=False)
+@app.route('/', methods=['POST'])
+def my_form_post():
+    global numSamples
+    numSamples = int (request.form['numSamples'])
+    numMaxSamples = maxRowsTable()
+    if (numSamples > numMaxSamples):
+        numSamples = (numMaxSamples-1)
+    time, temp, hum = getLastData()
+    templateData = {
+	  	'time'	: time,
+      		'temp'	: temp,
+      		'hum'	: hum,
+      		'numSamples'	: numSamples
+	}
+    return render_template('index.html', **templateData)
+
+
+@app.route('/plot/temp')
+def plot_temp():
+	times, temps, hums = getHistData(numSamples)
+	ys = temps
+	fig = Figure()
+	axis = fig.add_subplot(1, 1, 1)
+	axis.set_title("Temperature [°C]")
+	axis.set_xlabel("Samples")
+	axis.grid(True)
+	xs = range(numSamples)
+	axis.plot(xs, ys)
+	canvas = FigureCanvas(fig)
+	output = io.BytesIO()
+	canvas.print_png(output)
+	response = make_response(output.getvalue())
+	response.mimetype = 'image/png'
+	return response
+
+
+@app.route('/plot/hum')
+def plot_hum():
+	times, temps, hums = getHistData(numSamples)
+	ys = hums
+	fig = Figure()
+	axis = fig.add_subplot(1, 1, 1)
+	axis.set_title("Humidity [%]")
+	axis.set_xlabel("Samples")
+	axis.grid(True)
+	xs = range(numSamples)
+	axis.plot(xs, ys)
+	canvas = FigureCanvas(fig)
+	output = io.BytesIO()
+	canvas.print_png(output)
+	response = make_response(output.getvalue())
+	response.mimetype = 'image/png'
+	return response
