4976b90bd3742e8232a53b15dfb60a7a6a4526ee
joeriroijenga <roijenga.joeri@gmail.com>
2020-11-08 14:59:43 +0100

    recursive use fix

--- WebServer/run.py
+++ WebServer/run.py
@@ -8,10 +8,12 @@ app = Flask(__name__)
 import sqlite3
 
 
-def connectDB():
+def setupConnection():
 	global conn
 	global curs
 
+
+def connectDB():
 	conn = sqlite3.connect('../espData.db')
 	curs = conn.cursor()
 
@@ -68,6 +70,7 @@ if (numSamples > 101):
 # main route 
 @app.route("/")
 def index():
+	setupConnection()
 	
 	time, temp, hum = getLastData()
 	templateData = {
@@ -76,14 +79,18 @@ def index():
       'hum'			: hum,
       'numSamples'	: numSamples
 	}
+
 	return render_template('index.html', **templateData)
 
 
 @app.route('/', methods=['POST'])
 def my_form_post():
+    setupConnection()
+
     global numSamples 
     numSamples = int (request.form['numSamples'])
     numMaxSamples = maxRowsTable()
+    
     if (numSamples > numMaxSamples):
         numSamples = (numMaxSamples-1)
     
@@ -95,43 +102,56 @@ def my_form_post():
       'hum'			: hum,
       'numSamples'	: numSamples
 	}
+
     return render_template('index.html', **templateData)
 	
 	
 @app.route('/plot/temp')
 def plot_temp():
+	setupConnection()
+
 	times, temps, hums = getHistData(numSamples)
 	ys = temps
 	fig = Figure()
+	
 	axis = fig.add_subplot(1, 1, 1)
 	axis.set_title("Temperature [°C]")
 	axis.set_xlabel("Samples")
 	axis.grid(True)
 	xs = range(numSamples)
 	axis.plot(xs, ys)
+	
 	canvas = FigureCanvas(fig)
 	output = io.BytesIO()
 	canvas.print_png(output)
+	
 	response = make_response(output.getvalue())
 	response.mimetype = 'image/png'
+	
 	return response
 
 @app.route('/plot/hum')
 def plot_hum():
+	setupConnection()
+
 	times, temps, hums = getHistData(numSamples)
 	ys = hums
 	fig = Figure()
+	
 	axis = fig.add_subplot(1, 1, 1)
 	axis.set_title("Humidity [%]")
 	axis.set_xlabel("Samples")
 	axis.grid(True)
 	xs = range(numSamples)
 	axis.plot(xs, ys)
+	
 	canvas = FigureCanvas(fig)
 	output = io.BytesIO()
 	canvas.print_png(output)
+	
 	response = make_response(output.getvalue())
 	response.mimetype = 'image/png'
+	
 	return response
 
 if __name__ == "__main__":
