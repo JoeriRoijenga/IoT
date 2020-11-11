e390eb655819fc8ec2b39dfebc432f9da7fee0e0
joeriroijenga <roijenga.joeri@gmail.com>
2020-11-11 17:27:15 +0100

    changed format threadsg

--- Start.py
+++ Start.py
@@ -5,3 +5,7 @@ from threading import Thread
 if __name__ == '__main__':
     # Thread(target = run.start).start()
     # Thread(target = data_handler).start()
+    T1 = Thread(target=run.start, args=())
+    # T2 = Thread(target=data_handler, args=())
+    T1.start()
+    # T2.start()
