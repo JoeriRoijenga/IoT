1d2e51eec111901be5a8ad37a959520c5784c62c
joeriroijenga <roijenga.joeri@gmail.com>
2020-11-11 17:30:46 +0100

    removed thread for handler

--- Start.py
+++ Start.py
@@ -3,9 +3,7 @@ import data_handler as handler
 from threading import Thread
 
 if __name__ == '__main__':
-    # Thread(target = run.start).start()
-    # Thread(target = data_handler).start()
     T1 = Thread(target=server.start, args=())
-    T2 = Thread(target=handler, args=())
+    # T2 = Thread(target=handler, args=())
     T1.start()
-    T2.start()
+    # T2.start()
