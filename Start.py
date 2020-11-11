a6c47c234f40a8180715eb4691e794f248035b7e
joeriroijenga <roijenga.joeri@gmail.com>
2020-11-11 17:33:18 +0100

    added thread handler

--- Start.py
+++ Start.py
@@ -4,6 +4,6 @@ from threading import Thread
 
 if __name__ == '__main__':
     T1 = Thread(target=server.start, args=())
-    # T2 = Thread(target=handler, args=())
+    T2 = Thread(target=handler.start, args=())
     T1.start()
-    # T2.start()
+    T2.start()
