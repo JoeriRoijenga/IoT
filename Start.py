2f55f64fbeb03554ea9582d9d6e1f35113129db5
joeriroijenga <roijenga.joeri@gmail.com>
2020-11-11 17:24:02 +0100

    changed start

--- Start.py
+++ Start.py
@@ -1,7 +1,7 @@
 import WebServer.run
-import data_handler
+# import data_handler
 from threading import Thread
 
 if __name__ == '__main__':
-    Thread(target = run.start).start()
+    # Thread(target = run.start).start()
     # Thread(target = data_handler).start()
