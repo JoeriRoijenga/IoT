73799aefed45a9655491658fa08b18ab794a1902
joeriroijenga <roijenga.joeri@gmail.com>
2020-11-11 17:28:48 +0100

    added datahandler

--- Start.py
+++ Start.py
@@ -1,11 +1,11 @@
-import WebServer.run as run
+import WebServer.run as server
 # import data_handler
 from threading import Thread
 
 if __name__ == '__main__':
     # Thread(target = run.start).start()
     # Thread(target = data_handler).start()
-    T1 = Thread(target=run.start, args=())
-    # T2 = Thread(target=data_handler, args=())
+    T1 = Thread(target=server.start, args=())
+    T2 = Thread(target=data_handler, args=())
     T1.start()
-    # T2.start()
+    T2.start()
