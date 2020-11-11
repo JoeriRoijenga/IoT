757c80958ad9e21b19115d084631104aaa676bf5
joeriroijenga <roijenga.joeri@gmail.com>
2020-11-11 18:24:47 +0100

    changed css

--- WebServer/static/javascript.js
+++ WebServer/static/javascript.js
@@ -2,28 +2,28 @@ setTimeout(function(){
 	window.location.reload(1);
 }, 30000);
 
-var g1, g2;
-document.addEventListener("DOMContentLoaded", function(event) {
-	g1 = new JustGage({
-		id: "g1",
-		value: {{temp}},
-		valueFontColor: "black",
-		titleFontColor: "black",
-		min: -10,
-		max: 50,
-		title: "Temperature",
-		label: "Celcius"
-	});
+// var g1, g2;
+// document.addEventListener("DOMContentLoaded", function(event) {
+// 	g1 = new JustGage({
+// 		id: "g1",
+// 		value: {{temp}},
+// 		valueFontColor: "black",
+// 		titleFontColor: "black",
+// 		min: -10,
+// 		max: 50,
+// 		title: "Temperature",
+// 		label: "Celcius"
+// 	});
 
-	g2 = new JustGage({
-		id: "g2",
-		value: {{hum}},
-		valueFontColor: "black",
-		titleFontColor: "black",
-		min: 0,
-		max: 100,
-		title: "Humidity",
-		label: "%"
-	});
+// 	g2 = new JustGage({
+// 		id: "g2",
+// 		value: {{hum}},
+// 		valueFontColor: "black",
+// 		titleFontColor: "black",
+// 		min: 0,
+// 		max: 100,
+// 		title: "Humidity",
+// 		label: "%"
+// 	});
 
-});
+// });
