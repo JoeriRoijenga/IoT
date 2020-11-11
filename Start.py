f365faa5d0cf4ab61d4a02e14c0a6d85e7d7676a
joeriroijenga <roijenga.joeri@gmail.com>
2020-11-11 17:14:07 +0100

    changed multithread:

--- Start.py
+++ Start.py
@@ -2,5 +2,8 @@ import WebServer.run
 import data_handler
 from threading import Thread 
 
-thread = Thread(target = data_handler)
-thread = Thread(target = run)
+thread_handler = Thread(target = data_handler)
+thread_run = Thread(target = run)
+
+thread_handler.start()
+thread_run.start()
