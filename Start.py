1a5a663604e9e42b10b9f8e87a431411e65cf1da
joeriroijenga <roijenga.joeri@gmail.com>
2020-11-11 17:30:05 +0100

    added alias for handler

--- Start.py
+++ Start.py
@@ -1,11 +1,11 @@
 import WebServer.run as server
-import data_handler
+import data_handler as handler
 from threading import Thread
 
 if __name__ == '__main__':
     # Thread(target = run.start).start()
     # Thread(target = data_handler).start()
     T1 = Thread(target=server.start, args=())
-    T2 = Thread(target=data_handler, args=())
+    T2 = Thread(target=handler, args=())
     T1.start()
     T2.start()
