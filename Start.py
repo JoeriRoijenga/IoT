489fb1f94f9fe8bef0eab423073afbd671e1bb81
joeriroijenga <roijenga.joeri@gmail.com>
2020-11-11 17:22:27 +0100

    removed datahandler

--- Start.py
+++ Start.py
@@ -4,4 +4,4 @@ from threading import Thread
 
 if __name__ == '__main__':
     Thread(target = run.start).start()
-    Thread(target = data_handler).start()
+    # Thread(target = data_handler).start()
